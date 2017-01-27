91b5cb5368c7504f1d4527f98fa90345ec83efe1
ryanthomas92 <rthoma11@villanova.edu>
2017-01-27 15:10:37 -0800

    created a post route and connected to the controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
+
+  get '/users', to: 'users#index'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
 end
