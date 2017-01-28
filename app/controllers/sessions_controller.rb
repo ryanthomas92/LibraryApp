42199da19df7596fd456bde85aaf3b00658867d2
ryanthomas92 <rthoma11@villanova.edu>
2017-01-27 16:33:53 -0800

    finished part one, sessions display error and success login/logout messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,18 @@ class SessionsController < ApplicationController
     @user = User.confirm(user_params)
     if @user
       login(@user)
+      flash[:notice] = "Successfully logged in!"      # <-- Add into flash in appropriate application.html.erb page
       redirect_to @user
     else
+      flash[:error] = "Incorrect email or password!"  # <-- Add into flash in appropriate application.html.erb page
       redirect_to login_path
     end
   end
+
+  def destroy
+    logout
+    flash[:notice] = "Successfully logged out!"       # <-- Add into flash in appropriate application.html.erb page
+    redirect_to root_path
+  end
+
 end
